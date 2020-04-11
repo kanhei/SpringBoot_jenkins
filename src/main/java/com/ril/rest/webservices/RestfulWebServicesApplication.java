97240a06f4354d53d4607227d5f88e80ce68a9bd
Kanhei Padhy <kanhei.padhy@synapse.com>
2020-04-11 20:34:24 +0530

    seconf commit

--- src/main/java/com/ril/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/ril/rest/webservices/RestfulWebServicesApplication.java
@@ -1,13 +1,18 @@
 package com.ril.rest.webservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(RestfulWebServicesApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		log.info("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 	}
 
 }
